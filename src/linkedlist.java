7a17c8d9d2a0aa6e8076393a4aca9c759ac06803
HAIDER NAFEES <haidernafees93@gmail.com>
2019-11-22 00:09:16 -0500

    Fixing project (#3)
    * fixed linked list
    
    * removed unnecessary text
    
    * deleted //search a node

--- src/linkedlist.java
+++ src/linkedlist.java
@@ -139,6 +139,22 @@ public class linkedlist {
         }
     }
     
-    //search a Node
+    // addition of linked_list
+    public void addition(linkedlist a, linkedlist b)
+    {
+        Node h = a.head;
+        Node h1 = b.head;
+        Node temp = new Node(0);
+        int quotient = 0;
 
+        while(h != null)
+        {
+            temp.data = h.data + h1.data + quotient;
+            quotient = temp.data / 10;
+            temp.data = temp.data % 10;
+            add(temp.data);
+            h = h.next;
+            h1 = h1.next;
+        }
+    }
 }
--- src/palindrome_125.java
+++ src/palindrome_125.java
@@ -1,9 +1,8 @@
-
 import java.util.ArrayList;
 
 public class palidrom_125 {
-    
-     public boolean isSentencePalindrome(){   
+
+    public boolean isSentencePalindrome() {
         String s = "race a car";
         s = s.replaceAll("[^a-zA-Z0-9]", "");
         s = s.toLowerCase();
@@ -12,14 +11,11 @@ public class palidrom_125 {
         ArrayList<Character> original = new ArrayList<Character>();
         ArrayList<Character> palindrome = new ArrayList<Character>();
 
-        for(int i = s.length()-1; i >= 0; i--)
-        {
+        for (int i = s.length() - 1; i >= 0; i--) {
             palindrome.add(s.charAt(i));
             original.add(s.charAt(count));
             count++;
         }
-
-        boolean result;
-
+        return true;
     }
 }
