f820be9807f7e381bac26980565d31351604bd26
Haider Nafees <haidernafees93@gmail.com>
2019-11-22 00:22:15 -0500

    deleting and rebuilding palindrom

--- src/palindrom.java
+++ src/palindrom.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 
-public class palidrom_125 {
-
-    public boolean isSentencePalindrome() {
+public class palindrom
+{
+    public boolean isSentencePalindrome()
+    {
         String s = "race a car";
         s = s.replaceAll("[^a-zA-Z0-9]", "");
         s = s.toLowerCase();
@@ -11,10 +12,11 @@ public class palidrom_125 {
         ArrayList<Character> original = new ArrayList<Character>();
         ArrayList<Character> palindrome = new ArrayList<Character>();
 
-        for (int i = s.length() - 1; i >= 0; i--) {
-            palindrome.add(s.charAt(i));
-            original.add(s.charAt(count));
-            count++;
+        for (int i = s.length() - 1; i >= 0; i--)
+        {
+             palindrome.add(s.charAt(i));
+             original.add(s.charAt(count));
+             count++;
         }
         return true;
     }
