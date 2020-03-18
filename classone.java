ed7d947d531b22329d104b3c28c1dc558560833e
simran794 <39591939+simran794@users.noreply.github.com>
2020-03-17 23:22:07 -0400

    Added three Simmy made java files

---
+++ classone.java
@@ -0,0 +1,46 @@
+// public class classone{
+
+//     public class book{
+//         String name;
+//         String author;
+//         int year_published;
+//         int number_of_pages;
+//         String genre;
+//         Boolean read_before;
+//         book pointer;
+
+//         book(String b){
+//             this.name = b;
+//             this.pointer = null;
+//         }
+        
+//     }
+
+//     private book firstbook;
+   
+//     public void add(String x){
+//         book temp = new book(x);
+//         if(firstbook==null){firstbook=temp;}
+//         else{
+//             book copied = firstbook;
+//             while(copied.pointer!= null){
+//                 copied = copied.pointer;
+//             }copied.pointer=temp;
+//         }
+
+//     }
+
+//     public void print(){
+//         // public void print(){
+//         //     car headCopy1 = head;
+//         //     while(headCopy1.location != null){
+//         //         System.out.print(headCopy1.number + " -> ");
+//         //         headCopy1 = headCopy1.location;
+//         //     }
+//         //     System.out.println(headCopy1.number);
+//         //     System.out.println();
+//         // }
+//         book copied1 = firstbook;
+//         while
+//     }
+// }
--- unique.java
+++ unique.java
@@ -1,21 +1,35 @@
-import java.util.HashSet;
+import java.util.*;
 
-public class unique{
-    public static boolean isUnique(String str)
-    {
-        str = str.replaceAll("[^a-zA-Z0-9]", "");
-        HashSet<Character> unique = new HashSet<>();
-        for(int i = 0; i < str.length(); i++)
-        {
-            if(!unique.contains(str.charAt(i)))
-            {
-                unique.add(str.charAt(i));
+public class unique {
+    
+    public static boolean uniqueOccurrences(final int[] arr) {
+        HashMap<Integer, Integer> y = new HashMap<>();
+        HashSet<Integer> x = new HashSet<>();
+        
+        int len = arr.length;
+        for(int i=0; i < len; i++){
+           if (!y.containsKey(arr[i])){
+            y.put(arr[i],1);
+           }
+           else {
+                y.put(arr[i], y.get(arr[i])+1);
+           }
+        }
+        for (Integer key : y.values()){
+            // System.out.println(key);
+            if(!x.contains(key)){
+                x.add(key);
             }
-            else
-            {
+            else{
                 return false;
             }
         }
         return true;
     }
+// 1,2,2,1,1,3
+public static void main(String [] args){
+        int[] array = new int[]{-3,0,1,-3,1,1,1,-3,10,0};
+        System.out.println(uniqueOccurrences(array));
+    }
+
 }
